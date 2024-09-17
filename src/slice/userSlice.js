77bbfb37608d2de2c731cae795d7c2dbdf672ed6
Aamir-Yousaf <aamir.yousaf.dev@gmail.com>
2024-09-17 14:58:19 +0500

    fixed login error

--- src/slice/userSlice.js
+++ src/slice/userSlice.js
@@ -3,36 +3,36 @@ import { toast } from "react-toastify";
 
 const userSlice = createSlice({
   name: "user",
-  isLoggedIn:false,
+  isLoggedIn: false,
   initialState: {
     isLoggedIn: false,
     user: {},
-    currency: "PKR",  // Initialize with PKR currency as default. You can replace this with your preferred currency.
+    currency: "PKR", // Initialize with PKR currency as default. You can replace this with your preferred currency.
   },
-  
+
   reducers: {
     setUserLogin: (state, action) => {
       return (state = {
         isLoggedIn: true,
         data: action.payload,
-        currency: state.Currency,
+        currency: state.currency,
       });
     },
     LogoutUser: (state) => {
-      return (state={
+      return (state = {
         isLoggedIn: false,
         user: {},
-        currency: state.Currency,
+        currency: state.currency,
       });
     },
     changeCurrency: (state, action) => {
       return (state = {
-       ...state,
+        ...state,
         currency: action.payload,
       });
     },
   },
 });
 
-export const { setUserLogin,LogoutUser , changeCurrency} =userSlice.actions;
+export const { setUserLogin, LogoutUser, changeCurrency } = userSlice.actions;
 export default userSlice.reducer;
